54a3171344644dce7e7ff3827703c21a5e5a5666
Jarvanstyle <jarvanstyle@gmail.com>
2020-04-16 18:04:13 +1000

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -27,7 +27,7 @@ while cap.isOpened():
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=3)
-    dilated, contours, _ = cv2.findContours(
+    dilated, contours = cv2.findContours(
         dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     count = 0
